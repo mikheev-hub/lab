6dc1e45d836b37af352239266f3b5ba3956ec304
mikheev-hub <pavel-laz2010@mail.ru>
2019-12-07 12:13:59 +0300

    Update

--- lab1/src/7-sigment.c
+++ lab1/src/7-sigment.c
@@ -22,13 +22,13 @@ void extiBegin()
 {
     RCC->APB2ENR |=   (RCC_APB2ENR_IOPCEN | RCC_APB2ENR_AFIOEN);
 
-    EXTI->FTSR      &=~ EXTI_FTSR_TR0;    // Triggered on falling signal
+    EXTI->FTSR      &=~ EXTI_FTSR_TR0;                             // Triggered on falling signal
     EXTI->RTSR      |=  EXTI_RTSR_TR0;
     AFIO->EXTICR[0] |=  AFIO_EXTICR1_EXTI0_PC;
 
-    EXTI->PR        |=  EXTI_PR_PR0;	    //	flag of interrupt (call of interrupt) 				
-    EXTI->IMR       |=  EXTI_IMR_MR0;			//  access of interrup of appropriate channel 		
-    EXTI->EMR       |=  EXTI_EMR_MR0;     //  event on channel 
+    EXTI->PR        |=  EXTI_PR_PR0;	                             //	flag of interrupt (call of interrupt) 				
+    EXTI->IMR       |=  EXTI_IMR_MR0;		 	                         //  access of interrup of appropriate channel 		
+    EXTI->EMR       |=  EXTI_EMR_MR0;                              //  event on channel 
 
     NVIC_EnableIRQ(EXTI0_IRQn);
 }
@@ -44,16 +44,16 @@ void displayWrite(uint8_t cnt)
     GPIOA->CRL    = 0x33333333;  
     
 /*
-				 Px4
+         Px4
 			 _______
-			|       |
-	Px5 |  Px6  | Px3
-			|_______|		
-			|       |
-	Px0 |       | Px2
-			|_______|
+      |       |
+  Px5 |  Px6  | Px3
+      |_______|		
+      |       |
+  Px0 |       | Px2
+      |_______|
 
-				 Px1
+         Px1
 */  
     
 		uint16_t display[10]={
@@ -66,7 +66,7 @@ void displayWrite(uint8_t cnt)
                              GPIO_ODR_ODR4 | GPIO_ODR_ODR5 | GPIO_ODR_ODR6 | GPIO_ODR_ODR2 | GPIO_ODR_ODR1 | GPIO_ODR_ODR0,                 //6
                              GPIO_ODR_ODR4 | GPIO_ODR_ODR3 | GPIO_ODR_ODR2,                                                                 //7
                              GPIO_ODR_ODR4 | GPIO_ODR_ODR5 | GPIO_ODR_ODR3 | GPIO_ODR_ODR6 | GPIO_ODR_ODR2 | GPIO_ODR_ODR1 | GPIO_ODR_ODR0, //8
-                             GPIO_ODR_ODR4 | GPIO_ODR_ODR3 | GPIO_ODR_ODR5 | GPIO_ODR_ODR6 | GPIO_ODR_ODR2 | GPIO_ODR_ODR1  //9
+                             GPIO_ODR_ODR4 | GPIO_ODR_ODR3 | GPIO_ODR_ODR5 | GPIO_ODR_ODR6 | GPIO_ODR_ODR2 | GPIO_ODR_ODR1                  //9
                          };
     GPIOA->ODR = display[cnt];		
 }
